5f175718c56282be24760ec8b1714ee839656fd0
krenee411 <kelci.morgan@bryanuniversity.edu>
2022-07-22 18:43:00 -0500

    dataStructures Added

--- week2/datastructures.py
+++ week2/datastructures.py
@@ -1,14 +1,14 @@
 Data1 = (7, False, "Apple", True, 7, 98.6) #Tuple
 print(" ----------  Data 1 :")
-print(len(Data1)) #count //6
-print(Data1[3]) # fourth value // True
+print(len(Data1)) #count 
+print(Data1[3]) # fourth value 
 
 findSeven= 0
 for n in Data1:
     if n ==7 :
         findSeven += 1
 
-print(findSeven) #7 appears //2
+print(findSeven) #7 appears
 
 #-------------------------------------------------------------------------------
 Data2 = {"July", 4, 8, "Tango", 4.3, "Bingo"} #Set
@@ -22,10 +22,10 @@ print(Data2)  #print data
 Data3 = ["A", 7, -1, 3.14, True, 7]  #List
 
 print(" ----------  Data 3 :")
-Data3.reverse()
-Data3[1] = "B"
-Data3.pop()
-print(Data3)
+Data3.reverse()  # reverse
+Data3[1] = "B" #replace second value
+Data3.pop()  #remove last item
+print(Data3)  #print
 #-------------------------------------------------------------------------------
 Data4 ={
     " name": "Joe",
@@ -35,11 +35,11 @@ Data4 ={
 } #Dictionary
 
 print(" ----------  Data 4 :")
-Data4["active"] = False
-Data4["address"] = "123 West Main Street"
-x=Data4.get("hourly_wage")
-print(x * 40)
-print(Data4)
+Data4["active"] = False  #change active to False
+Data4["address"] = "123 West Main Street"  #add Address
+x=Data4.get("hourly_wage") #get hourly_wage from dictionary 
+print(x * 40)  # get pay for 40 hours
+print(Data4)  #print
 
 
 
