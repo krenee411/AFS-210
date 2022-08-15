5a3c842f1d9bd38a636a2ec5ddb75e961ab69389
krenee411 <kelci.morgan@bryanuniversity.edu>
2022-08-14 23:23:59 -0500

    megasort added

---
+++ week5/sorting/mergesort.py
@@ -0,0 +1,49 @@
+def mergeSort(nlist):
+    print("Splitting ",nlist)
+    # insert your code to complete the mergeSort function
+    
+    list_length = len(nlist)
+
+    if list_length == 1:
+        return nlist
+
+    mid_point = list_length // 2
+
+    left_side = mergeSort(nlist[:mid_point])
+
+    right_side = mergeSort(nlist[mid_point:])
+
+    nlist = merge(nlist, left_side, right_side)
+    
+    print("Merging ", nlist)
+    
+    return nlist
+
+myList = [ 55, 31, 26, 20, 63, 7, 51, 74, 81, 40 ]
+
+
+def merge(nlist,lefthalf,righthalf):
+    i=j=k=0       
+    while i < len(lefthalf) and j < len(righthalf):
+        if lefthalf[i] < righthalf[j]:
+            nlist[k]=lefthalf[i]
+            i=i+1
+        else:
+            nlist[k]=righthalf[j]
+            j=j+1
+        k=k+1
+
+    while i < len(lefthalf):
+        nlist[k]=lefthalf[i]
+        i=i+1
+        k=k+1
+
+    while j < len(righthalf):
+        nlist[k]=righthalf[j]
+        j=j+1
+        k=k+1
+    return nlist
+
+mergeSort(myList)
+
+print('Sorted: ', myList)
